4306c4734736b4e5fe04d76e9069028868a73baa
krishnadas514 <168108489+krishnadas514@users.noreply.github.com>
2024-04-28 21:07:19 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+class test1 {
+    
+    public static void main(String[] args){
+    child c1 = new child();
+    parent p1 = new parent();
+    
+    p1.money();
+    c1.liability();
+    c1.money();
+    }
+}
